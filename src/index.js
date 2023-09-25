46417c56bbb385104388d8d80ac1ef5138bfae51
jaehun.jang <jhonbeetbox@naver.com>
2023-09-25 14:35:39 +0900

    Add homepage

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
-    {/* <React.StrictMode> */}
-    <App />
-    {/* </React.StrictMode> */}
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
