f5732cdeaafefcabdcbd9d9f7aa5365e97ee8943
jaehun.jang <jhonbeetbox@naver.com>
2023-09-26 12:51:44 +0900

    Add homepage

--- src/App.js
+++ src/App.js
@@ -9,27 +9,25 @@ import MyNavBar from './components/MyNavBar';
 import GIFConverter from "./routes/GifConverter";
 
 function App() {
-  console.log(process.env.PUBLIC_URL)
+  console.log(process.env.PUBLIC_URL + "/gifconverter")
   return (
     <div className="App">
       <header className="App-header">
         <MyNavBar/>
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
-          {/* <Router> */}
+        <Router>
             <Routes>
-              <Route path="/" element={<GIFConverter />}>
+              <Route path={process.env.PUBLIC_URL + "/"} element={<GIFConverter />}>
               </Route>
             </Routes>
             <Routes>
-              <Route path="/gifconverter" element={<GIFConverter />}>
+              <Route path={process.env.PUBLIC_URL + "/gifconverter"} element={<GIFConverter />}>
               </Route>
             </Routes>
             <Routes>
-              <Route path="/tab2" element={<h1>tab2</h1>}>
+              <Route path={process.env.PUBLIC_URL + "/tab2"} element={<h1>tab2</h1>}>
               </Route>
             </Routes>
-          {/* </Router> */}
-        </BrowserRouter>
+        </Router>
       </header>
     </div>
   );
--- src/components/MyNavBar.js
+++ src/components/MyNavBar.js
@@ -8,14 +8,14 @@ function MyNavBar() {
   return (
     <Navbar expand="lg" bg="dark" data-bs-theme="dark" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="/">BlogToolTool</Navbar.Brand>
+        <Navbar.Brand href={process.env.PUBLIC_URL+"/"}>BlogToolTool</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/gifconverter">GIF 변환기</Nav.Link>
+          <Nav.Link href={process.env.PUBLIC_URL+"/"}>Home</Nav.Link>
+          <Nav.Link href={process.env.PUBLIC_URL+"/gifconverter"}>GIF 변환기</Nav.Link>
           <NavDropdown title="이미지 변환기" id="navbarScrollingDropdown">
-            <Nav.Link href="/tab2">워터마크 추가</Nav.Link>
+            <Nav.Link href={process.env.PUBLIC_URL+"/tab2"}>워터마크 추가</Nav.Link>
           </NavDropdown>
         </Nav>
         </Navbar.Collapse>
