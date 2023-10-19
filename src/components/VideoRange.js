2a985a2e4a478ba0ede9e0818c2e26626f3c8d2f
jaehun.jang <jhonbeetbox@naver.com>
2023-10-19 10:34:59 +0900

    range bar css

--- src/components/VideoRange.js
+++ src/components/VideoRange.js
@@ -1,12 +1,13 @@
 import Container from "react-bootstrap/esm/Container";
-import styles from '../styles/VideoRange.module.css'
+import videorangestyle from '../styles/VideoRange.module.css';
 
 function VideoRange({ maxtime, starttime, endtime, onChange, abled }) {
     return (
-        <Container style={{ width: '100%', height: '30px'
-        , display: 'flex', justifyContent: 'center'
-        , position : 'relative', top : '0px', left : '0px'
-        }}>
+        // <Container style={{ width: '100%', height: '30px'
+        // , display: 'flex', justifyContent: 'center'
+        // , position : 'relative', top : '0px', left : '0px'
+        // }}>
+        <Container className={videorangestyle.videorangecontainer}>
             <input id='InputRange1' type="range" step={0.001}
                 min="0" max={maxtime} value={starttime} onChange={onChange} disabled={abled} />
             <input id='InputRange2' type="range" step={0.001}
